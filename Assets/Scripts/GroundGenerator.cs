ed6f5d2eb89bdbed96275964dea609787b348eb2
weislex <bogdansadovan@yahoo.com>
2021-05-21 13:24:29 +0300

    added game controls text

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -17,7 +17,7 @@ public class GroundGenerator : MonoBehaviour
 
     //Start is called before the first frame update
     void Start(){
-        for (int i = 0; i < 15; i++){
+        for (int i = 0; i < 100; i++){
             if(i < 5)
                 SpawnTile(false);
              else 
